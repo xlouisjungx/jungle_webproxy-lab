6f1285eb2e0d8ff8a6627f11e815cdfc66153466
xlouisjungx <cutaandcrazy@gmail.com>
2025-05-07 14:44:08 +0000

    코드 설명 주석 추가

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -16,6 +16,29 @@ void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
+/*
+-------------------------------------------------------
+| 클라이언트 요청 → Accept() → doit()                     |
+|                                                     |
+| doit()                                              |
+|  ├─ 읽기: 요청 라인 + 헤더                              |
+|  ├─ URI 파싱 → 정적 or 동적                            |
+|  ├─ 파일 존재 검사                                     |
+|  ├─ serve_static() or serve_dynamic()               |
+|  └─ 응답 전송 완료 → 소켓 닫기                           |
+-------------------------------------------------------
+|  이 코드의 전체 흐름은?                                  |
+|   1. 클라이언트가 요청을 보냄. (GET /index.html HTTP/1.0) |
+|   2. 서버가 요청을 읽고 파싱                              |
+|   3. 요청에 맞는 파일 또는 CGI 실행                       |
+|   4. 응답을 클라이언트에게 전송                            |
+-------------------------------------------------------
+
+*/
+
+// 서버는 커맨드라인 인자로 포트 번호를 받아야 함.
+// Open_listenfd(argv[1])으로 주어진 포트에 대해 리스닝 소켓 생성.
+// 무한루프로 클라이언트 연결을 계속 수락.
 int main(int argc, char **argv) { // 소켓 서버 생성 -> 클라이언트 연결 요청 수락 처리 -> 소켓 닫기 
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
@@ -31,6 +54,8 @@ int main(int argc, char **argv) { // 소켓 서버 생성 -> 클라이언트 연
   listenfd = Open_listenfd(argv[1]); // 포트에서 listen
   while (1) {
     clientlen = sizeof(clientaddr);
+
+    // 쿨라이언트와 연결되면 doit() 함수로 HTTP 요청 처리를 맡기고, 처리 후 소켓 닫기.
     connfd = Accept(listenfd, (SA *)&clientaddr, // 클라이언트 연결 수락
                     &clientlen);  // line:netp:tiny:accept
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
@@ -50,6 +75,8 @@ void doit(int fd) {
   rio_t rio;
 
   Rio_readinitb(&rio, fd);
+
+  // HTTP 요청 라인 읽기: GET /index.html HTTP/1.0
   Rio_readlineb(&rio, buf, MAXLINE);
   printf("Request headers:\n");
   printf("%s", buf);
@@ -60,8 +87,11 @@ void doit(int fd) {
   }
   read_requesthdrs(&rio);
 
+  // 정적인지 동적인이 콘텐츠를 판단
   is_static = parse_uri(uri, filename, cgiargs); // URI 파싱 -> 정적이냐 동적이냐를 판단
   if(stat(filename, &sbuf) < 0) { // 파일이 존재하는지 확인
+
+    // 잘못된 요청에 대해 HTML로 응답을 구성해 전송
     clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
     return;
   }
@@ -71,6 +101,8 @@ void doit(int fd) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
       return;
     }
+    // 정적 콘텐츠 처리 ex) HTML, 이미지 파일
+    // 메모리에 파일 매핑(MMAP) -> 클라이언트에게 전송
     serve_static(fd, filename, sbuf.st_size);
   }
   else { // 존재하는 파일이 동적이면
@@ -78,6 +110,9 @@ void doit(int fd) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
       return;
     }
+    // CGI 프로그램을 자식 프로세스로 fork해서 실행
+    // QUERY_STRING 환경변수 설정
+    // CGI 결과를 클라이언트에게 출력
     serve_dynamic(fd, filename, cgiargs);
   }
 }
@@ -116,6 +151,8 @@ void read_requesthdrs(rio_t *rp) {
 // URI가 정적인지 동적인지 판단하고, CGI인자도 파싱
 // /cgi-bin/이 포함되면 동적 요청
 // 아니면 정적 파일로 요청
+// 정적: /index.gtml -> ./index.html
+// 동적: cgi-bin/add.cgi?x=1&y=2 -> filename = ./cgi-bin/add.cgi, cgiargs = x=1&y=2
 int parse_uri(char *uri, char *filename, char *cgiargs) {
   char *ptr;
 
@@ -155,9 +192,11 @@ void serve_static(int fd, char *filename, int filesize) {
   printf("%s", buf);
 
   srcfd = Open(filename, O_RDONLY, 0);
+
+  // 파일을 mmap()으로 메모리에 매핑
   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
   Close(srcfd);
-  Rio_writen(fd, srcp, filesize); // 응답 전송
+  Rio_writen(fd, srcp, filesize); // 본문으로 응답 전송
   Munmap(srcp, filesize);
 }
 
@@ -183,20 +222,10 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
 
-  if(Fork() == 0) {
+  if(Fork() == 0) { // Fork() -> 자식 프로세스에서 환경 설정 및 execve()로 CGI 실행
     setenv("QUERY_STRING", cgiargs, 1);
-    Dup2(fd, STDOUT_FILENO);
+    Dup2(fd, STDOUT_FILENO); // 클라이언트 소켓을 stdout으로 바꾸기 -> CGI 출력이 클라이언트로 직접 감
     Execve(filename, emptylist, environ);
   }
   Wait(NULL);
 }
-
-/*
-
-이 코드의 전체 흐름은?
-1. 클라이언트가 요청을 보냄. (GET /index.html HTTP/1.0)
-2. 서버가 요청을 읽고 파싱
-3. 요청에 맞는 파일 또는 CGI 실행
-4. 응답을 클라이언트에게 전송
-
-*/
